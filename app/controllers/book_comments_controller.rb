fb8985ea50e7a6b44eab28c414e7eb5a69a754d6
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-03 09:10:38 +0000

    [Finish]応用課題3 完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,6 +3,8 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
+    comment.save
+    redirect_to request.referer
     
   end
 

818df03ecce31dd55e2e81293e9f054860ed9bab
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-05 10:23:53 +0000

    [Update]課題4 フォロー一覧追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,20 +3,20 @@ class RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
-  
+
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
-  
+
   def followings
-    user = User.find(params[user_id])
-    @users = user.followings
+    user = User.find(params[:user_id])
+    @users = user.following_user
   end
-  
+
   def followers
-    user = User.find(params[user_id])
-    @users = user.followers
+    user = User.find(params[:user_id])
+    @users = user.follower_user
   end
-  
+
 end

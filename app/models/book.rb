e22c8ec9414b9bff3a06e3318b3b1afd45bee730
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-08-08 06:35:35 +0000

    [Update]課題7a いいね多い順に表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,18 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
-  
-  
+
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
-  
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?", "#{word}")
@@ -25,5 +26,5 @@ class Book < ApplicationRecord
       @book = Book.all
     end
   end
-  
+
 end
